f389b6b98a6e5b9e59f68c1b20b871f30dc0e199
Gurumurthy2001 <gurumurthiat2019@gmail.com>
2023-12-23 12:46:07 +0530

    challenge,userprofile and activity models are created

--- fitness_app/models.py
+++ fitness_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Challenge(models.Model):
+  title=models.CharField(max_length=255)
+  description=models.TextField()
+  duration=models.IntegerField()
+  
+  def __str__(self):
+    return self.title
+  
+
+class UserProfile(models.Model):
+  user=models.OneToOneField(User,on_delete=models.CASCADE)
+  profile_pic= models.ImageField(upload_to='profile_pic/',blank=True,null=True)
+  bio_line= models.TextField()
+  
+  def __str__(self):
+    return self.user.username
+  
+  
+  
+class Activity(models.Model):
+  user=models.ForeignKey(User,on_delete=models.CASCADE)
+  challenge=models.ForeignKey(Challenge,on_delete=models.CASCADE)
+  date=models.DateField()
+  
+  def __str__(self):
+    return f'{self.user.username} - {self.challenge.title}'
+  
